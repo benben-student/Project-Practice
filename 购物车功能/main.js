5c4d643d726c2ba87d4d9fe0e0fdde2fdf4d6dd7
520_forever <a441832620@126.com>
2021-06-15 20:00:52 +0800

    购物车效果

--- 购物车功能/main.js
+++ 购物车功能/main.js
@@ -17,20 +17,40 @@ window.onload = function () {
     var checkAllInputs = document.getElementsByClassName("check-all");
     var selectedTotal = document.getElementsById("selectedTotal");
     var priceTotal = document.getElementsById("priceTotal");
+    var selectd=document.getElementById("selected");
+    var foot=document.getElementsById("foot");
+    var selectdViewList=document.getElementsById("selectdViewList");
+    var deleteAll=document.getElementsById("deleteAll");
     function getTotal() {
         var seleted = 0;
         var price = 0;
         for (var i = 0, len = tr.length; i < len; i++) {
             if (tr[i].getElementsByTagName("input")[0].checked) {
-                selected += parseInt(tr[i].getElementsByTagName("input")[1]);
+                tr[i].className="on";
+                seleted += parseInt(tr[i].getElementsByTagName("input")[1].value);
                 price += parseFloat(tr[i].cells[4].innerHTML);
+                HTMLstr+='<div><img src="'+tr[i].getElementsByTagName('img')[0].src+'"><span class=""del index="'+i+'">取消选择</span></div>';
+            }
+            else{
+                tr[i].className="";
             }
         }
         selectedTotal.innerHTML = seleted;
         priceTotal.innerHTML = price.toFixed(2);
+        selectdViewList.innerHTML=HTMLstr
+        if(selectd==0){
+            foot.className="foot";
+        }
 
     }
-    for (var i = 0,len = chenckInputs.length; i < len; i++) {
+    function getSubTotal(tr){
+        var tds=tr.cells;
+        var price =parseFloat(tds[2].innerHTML);
+        var count=parseInt(tr.getElementsByTagName("input")[1].value);
+        var SubTotal=parseFloat(price*count);
+        tds[4].innerHTML=SubTotal.toFixed(2);
+    }
+    for (var i = 0, len = chenckInputs.length; i < len; i++) {
         checkInputs[i].onclick = function () {
             if (this.className === "check-all check") {
                 for (var j = 0; j < checkInput.length; j++) {
@@ -38,12 +58,98 @@ window.onload = function () {
                 }
 
             }
-          if(this.checked==false){
-              for(var k=0;k<checkAllInputs.length;k++){
-                  checkAllInputs[k].checked=false;
-              }
-          }
+            if (this.checked == false) {
+                for (var k = 0; k < checkAllInputs.length; k++) {
+                    checkAllInputs[k].checked = false;
+                }
+            }
         }
         getTotal();
+}
+selected.onclick=function(){
+    if(foot.className=="foot"){
+        if(selectedTotal.innerHTML!=0){
+            foot.className="foot show";
+        }
     }
+    else{
+        foot.className="foot";
+    }
+}
+selectdViewList.onclick=function(e){
+    e=e||window.event;
+   var el=e.srcElement;
+   if(el.className=="del"){
+       var index=el.getAttribute("index");
+       var input=tr[index].getElementsByTagName("input")[0];
+       input.checked=false;
+       input.onclick();
+   }
+}
+for(var i=0;i<tr.length;i++){
+    tr[i].onclick=function(e){
+        e=e||window.event;
+        var el=e.srcElement;
+        var input=this.getElementsByTagName("input")[1];
+        var val=parseInt(input.value);
+        var reduce=this.getElementsById("span")[1];
+        switch(cls){
+            case "add":
+                input.value=val+1;
+                reduce.innerHTML="-";
+                getSubTotal(this);
+                break;
+                case "reduce":
+                    if(val >1){
+                        input.value=val-1;
+                    }
+                    if(input.value<=1){
+                        reduce.innerHTML="-";
+                    }
+                    getSubTotal(this);
+                    break;
+                    case "delete":
+                        var conf=confirm("你确定要删除吗");
+                        if(conf){
+                             this.parentNode.removeChild(this);
+                        }
+                        break;
+                    default:
+                        break;
+        }
+        getTotal(this);
+    }
+    tr[i].getElementsByTagName("input")[1].onkeyup=function(){
+        var val=parseInt(this.value);
+        var tr =   getSubTotal(this.parentNode.parentNode);
+        var reduce=tr.getElementsByTagName("span")[1];
+        if(isNaN(val)||val<1){
+            val=1;
+        }
+        this.value=val;
+        if(val<=1){
+            reduce.innerHTML="";
+        }else{
+            reduce.innerHTML="-";
+        }
+        getSubTotal(tr);
+        getTotal();
+    }
+  }
+deleteAll.onclick=function(){
+  if(selectedTotal.innerHTML!="0"){
+    var conf=confirm("确定要删除吗");
+    if(conf){
+         for(var i=0;i<tr.length;i++){
+             var input=tr[i].getElementsByTagName("input")[0];
+         if(input.checked){
+            tr[i].parentNode.removeChild(tr[i]);
+            i--;
+          }
+        }
+    }
+  }
+  }
+  checkAllInputs[0].checked=true;
+  checkAllInputs[0].onclick();
 }
